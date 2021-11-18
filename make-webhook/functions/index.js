00ac55449a054e45d0b0d0a3d0609198067f2e84
RVCONNEX\Yuttasak.R <in117dk54@gmail.com>
2021-11-18 17:03:36 +0700

    Tests

--- make-webhook/functions/index.js
+++ make-webhook/functions/index.js
@@ -1,9 +1,10 @@
 const functions = require("firebase-functions");
 const line = require("@line/bot-sdk");
-const request = require("request-promise");
 const express = require("express");
 const app = express();
 const region = "asia-southeast1";
+const request = require("request-promise");
+const structjson = require("./structjson");
 //========================================== Line ==============================================================
 const config = {
   channelAccessToken: functions.config().line.channel_access_token,
@@ -20,20 +21,21 @@ admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 //=========================================== Dialog flow ============================================================
 const dialogflow = require("@google-cloud/dialogflow");
-const { SessionsClient } = require("@google-cloud/dialogflow");
-const { get } = require("request");
+const { user } = require("firebase-functions/lib/providers/auth");
 const projectId = "chat-project-bef27";
 //======================================================================================================================
+
 
 //=========================================== handle event =============================================================
 async function handleEvent(req, event) {
   switch (event.type) {
-    case "message":
+    case "message": {
       switch (event.message.type) {
         case "text":
-          addChatHistory(req, event);
-          return;
+          await addChatHistory(req, event);
+          return await detectIntentApi(event);
       }
+    }
     default:
       throw new Error(`Unknown event: ${JSON.stringify(event)}`);
   }
@@ -41,42 +43,56 @@ async function handleEvent(req, event) {
 //============================================================================================================================
 
 //===================== function detectIntent api dialogflow .=====================================
-// async function detectIntentApi(req, event) {
-//   const sessionClient = new dialogflow.SessionsClient({
-//     projectId,
-//     keyFilename: "chat-project-bef27-72a3467da386.json",
-//   });
-//   const userId = event.source.userId;
-//   const userText = event.message.text;
-//   const sessionPath = sessionClient.projectAgentSessionPath(projectId, userId);
-//   console.log("1");
-//   const request = {
-//     session: sessionPath,
-//     queryInput: {
-//       text: {
-//         text: userText,
-//         languageCode: "th",
-//       },
-//     },
-//   };
-//   const response = await sessionClient.detectIntent(request).catch((err) => {
-//     console.log("detectIntent:", err);
-//     throw err;
-//   });
-//   return response[0];
-// }
+async function detectIntentApi(event) {
+  const sessionClient = new dialogflow.SessionsClient({
+    projectId,
+    keyFilename: "chat-project-bef27-72a3467da386.json",
+  });
+  const userId = event.source.userId;
+  console.log("Line Id : ", userId);
+  const message = event.message.text;
+  console.log("Text : ", message);
+  const sessionPath = sessionClient.projectAgentSessionPath(projectId, userId);
+  const data = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: message,
+        languageCode: "th",
+      },
+    },
+  };
+  console.log("Data to Bot : ", data);
+  const response = await sessionClient.detectIntent(data);
+  const messgae = response[0].queryResult.fulfillmentText
+    ? response[0].queryResult.fulfillmentText
+    : "ฉันไม่รู้ค่ะ";
+  return await replyToUser(event, messgae);
+}
+
+async function replyToUser(event, responseMasggase) {
+  return request({
+    method: "POST",
+    uri: `${LINE_MESSAGING_API}/reply`,
+    headers: LINE_HEADER,
+    body: JSON.stringify({
+      replyToken: event.replyToken,
+      messages: [{ type: "text", text: responseMasggase }],
+    }),
+  });
+}
 //======================================================================================================
 //==================================== function add chat history in firebase firestore ===============================================
 async function addChatHistory(req, event) {
   const userId = event.source.userId;
   const timestamp = event.timestamp;
-  const userText = event.message.text;
+  const message = event.message.text;
   const addchatHis = db
     .collection("chat-history")
     .doc()
     .set({
       userId: userId,
-      Message: userText,
+      Message: message,
       timestamp: timestamp,
     })
     .catch((err) => {
@@ -85,11 +101,12 @@ async function addChatHistory(req, event) {
 }
 //======================== line middle ware by Express js ====================================================
 app.post("/webhook", line.middleware(config), async (req, res) => {
-  Promise.all(
-    req.body.events.map((event) => {
-      return handleEvent(req, event);
+  const mapData = await Promise.all(
+    req.body.events.map(async (event) => {
+      return await handleEvent(req, event);
     })
   );
+  await Promise.all(mapData);
   res.status(200).end();
 });
 //========================================================================================================
